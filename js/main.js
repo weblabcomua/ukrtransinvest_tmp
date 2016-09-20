09f55027b50d92a4caec853edc6d07852e9f94a2
Alexandr Gorban <dorproekt@ukr.net>
2016-09-20 11:50:46 +0300

    about

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-  
-  
-  /*$("#myCarousel").ready(function(){
-    $("#myCarousel").carousel('cycle');
-  });*/
+$(document).ready(function(){
+   
+   var heightHeader = $("#myCarousel img").height();
+   //console.log(heightHeader);
+   $("header").css("height", heightHeader);
+   
+   
+   /*var buttonLeft = $(".btn-default:first").offset();
+   var buttonWidth = $(".btn-default:first").outerWidth();
+   var bodyWidth = $("body").width();
+   var marginMenu = Math.round(bodyWidth-(buttonLeft.left+buttonWidth));
+   $(".menu-list").css("margin-right",marginMenu+"px");*/
+   
+});
