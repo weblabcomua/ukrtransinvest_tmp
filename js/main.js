9349e8055cc239877e47bd5df762f0cfeee40828
Alexandr Gorban <dorproekt@ukr.net>
2016-09-21 12:27:56 +0300

    home final

--- js/main.js
+++ js/main.js
@@ -10,5 +10,23 @@ $(document).ready(function(){
    var bodyWidth = $("body").width();
    var marginMenu = Math.round(bodyWidth-(buttonLeft.left+buttonWidth));
    $(".menu-list").css("margin-right",marginMenu+"px");*/
+    
+    //home products
+   $("h3").click(function(){
+       $(".products li").removeClass("active");
+       $(this).parent().addClass("active");
+       var id = $(this).attr("id");
+       //console.log(id);
+       $(".product").removeClass("active");
+       $(".product."+id).addClass("active");
+   });
+   
+   $(".product img").hover(function(){
+       $(this).siblings(".desc").css("display","block");
+   });
+    
+    $("ul").hover(function(){
+        $(".desc").css("display","none");
+    });
    
 });
