e4481229550cdcc4d0e2cbf290d21e8cb357b5e2
Alexandr Gorban <dorproekt@ukr.net>
2016-09-22 11:38:30 +0300

    fixed menu

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
    $(".menu-list").css("margin-right",marginMenu+"px");*/
     
     //home products
-   $("h3").click(function(){
+   /*$("h3").click(function(){
        $(".products li").removeClass("active");
        $(this).parent().addClass("active");
        var id = $(this).attr("id");
@@ -27,6 +27,55 @@ $(document).ready(function(){
     
     $("ul").hover(function(){
         $(".desc").css("display","none");
-    });
+    });*/
+    
+    //home products
+   $("h3").hover(function(){
+       $(".products li").removeClass("active");
+       $(this).parent().addClass("active");
+       var id = $(this).attr("id");
+       //console.log(id);
+       $(".product").removeClass("active");
+       $(".product."+id).addClass("active");
+   });
+   
+   //menu scroll
+    var heightTopMenu = $(".menu").offset();
+    window.onscroll = function() {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if(scrolled >= 50){
+            $(".menu").addClass("menu-fixed");
+
+        }else{
+            $(".menu").removeClass("menu-fixed");
+        }
+    };
+    
+    /*$(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+        var heightTopMenu = $(".menu").offset();
+        
+        if(scrollTop >= heightTopMenu.top){
+            $(".menu").addClass("menu-fixed");
+        }else{
+            console.log(11111);
+        }
+        
+        
+//        if(scrollTop != 0){
+//            $("nav.navbar").addClass("nav-fixed");
+//            $(".scroll.top").fadeIn(1000);
+//            $(".submenu").slideUp(1000);
+//            $(".navbar-header p").fadeIn(1500);
+//            return false;
+//        } else {
+//            $("nav.navbar").removeClass("nav-fixed");
+//            $(".scroll.top").fadeOut(1000);
+//            $(".submenu").slideDown(1000);
+//            $(".navbar-header p").fadeOut(1000);
+//            return false;
+//        }
+    });*/
+   
    
 });
